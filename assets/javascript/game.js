5f082077c48577472b4dce426d3742afb81e6198
Tony Kuang <tonyjkuang@gmail.com>
2017-10-04 15:00:54 -0500

    <COMMENT>

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -3,24 +3,26 @@ var computerChoices = ["warriors","cavaliers","lakers","spurs","knicks","celtics
 "clippers","76ers","nets","pelicans","mavericks","wizards",
 "blazers","kings","suns","pistons","pacers","hawks",
 "timberwolves","nuggets","grizzlies","hornets","magic","jazz"];
+var letters = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u',
+'v','w','x','y','z','1','2','3','4','5','6','7','8','9','0'];
 var guesses = 5;
 var wins = 0;
 var guessesLeft = 5;
 var guessedLetters = [];
+var displayedAnswer = [];
 var letterToGuess = null;
 
 var answer = computerChoices[Math.floor(Math.random() * computerChoices.length)].split("");
 
+// Show letters user input
+
 document.onkeyup = function(event) {
+	var letters = document.querySelector('#usedGuesses');
 	var userGuess = event.key;
-	var numberOfGuesses = userGuess.length;
-	var storeGuesses = "";
-	for ( var j = 0; j < userGuess.length; j++) {
-		storeGuesses = storeGuesses + ", ";
-	}
-	document.querySelector('#usedGuesses').innerHTML = "Letters typed: " + storeGuesses;
+		letters.innerHTML = letters.innerHTML + " " + userGuess + ", ";	
 }
 
+//Generate underscores depending on the length of the answer
 
 function generateUnderscore() {
 	var numberOfUnderscores = answer.length;
@@ -28,20 +30,25 @@ function generateUnderscore() {
 	for ( var i = 0; i < numberOfUnderscores; i++) {
 		storeUnderscore = storeUnderscore + "_" + " ";
 	}
-	document.querySelector('#answer').innerHTML = "Current Word: " + storeUnderscore;
-
+	document.querySelector('#answer').innerHTML = storeUnderscore;
 }
 generateUnderscore();
 
+//Replace the underscore with the letter
+
+function fillInWordWithLetter(char) {
+	for ( var k = 0; k < answer.length; k++) {
+		if ( answer.charAt(k) == char) {
+			displayedAnswer[k] = char;
+		}
+	}
+}
 
 
 var updateGuessesLeft = function() {
 	document.querySelector('#guessLeft').innerHTML = "Number of guesses remained: " + guessesLeft;
 };
 
-var updateLetterToGuess = function() {
-  this.letterToGuess = this.computerChoices[Math.floor(Math.random() * this.computerChoices.length)];
-};
 
 var updateGuessesSoFar = function() {
   document.querySelector('#usedGuesses').innerHTML = "Letters typed: " + guessedLetters.join(', ');
@@ -59,41 +66,3 @@ var reset = function() {
 
 updateLetterToGuess();
 updateGuessesLeft();
-
-
-document.onkeyup = function(event) {
-	guessesLeft--;
-  var userGuess = event.key;
-
-  guessedLetters.push(userGuess);
-  updateGuessesLeft();
-  updateGuessesSoFar();
-
-	 for (var i =0; i < computerChoices.length; i++) {
-	 	if (guessLeft > 0) {
-	 		if (userGuess == computerChoices.charAt(0)) {
-	 			document.querySelector('#answer').innerHTML = "Current Word: " + 
-	 		}
-	 		else if (userGuess == computerChoices.charAt(2)) {
-	 			 
-	 		}
-	 	}
-	 	if (guessedLetters.charAt(i) == computerChoices) {
-	 		wins++;
-	 		document.querySelector('#wins').innerHTML = "Wins: " + wins;
-	 	}
-	 	if (guessesLeft > 0){
-	            if (userGuess == letterToGuess){
-	                wins++;
-	                
-	                reset();
-	            }
-	        }else if(guessesLeft == 0){
-	            // Then we will loss and we'll update the html to display the loss 
-	            losses++;
-	            document.querySelector('#losses').innerHTML = "Losses: " + losses;
-	            alert("Sorry, you're not psychic, maybe try again?");
-	            // Then we'll call the reset. 
-	            reset();
-	        }
-	}
