84018bb2ed2e7ad0b33755b97359df0b9ad04da7
Tony Kuang <tonyjkuang@gmail.com>
2017-10-04 23:12:16 -0500

    <COMMENT>

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -3,9 +3,6 @@ var computerChoices = ["warriors","cavaliers","lakers","spurs","knicks","celtics
 "clippers","76ers","nets","pelicans","mavericks","wizards",
 "blazers","kings","suns","pistons","pacers","hawks",
 "timberwolves","nuggets","grizzlies","hornets","magic","jazz"];
-var letters = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u',
-'v','w','x','y','z','1','2','3','4','5','6','7','8','9','0'];
-var useGuess = [];
 var guesses = 5;
 var wins = 0;
 var guessesLeft = 5;
@@ -20,47 +17,45 @@ var answer = computerChoices[Math.floor(Math.random() * computerChoices.length)]
 document.onkeyup = function(event) {
 	var letters = document.querySelector('#usedGuesses');
 	var userGuess = event.key;
-		for ( var a = 0; a < letters.length; a++) {
-			if ( userGuess == letters[a] ) {
-			letters.innerHTML = letters.innerHTML + " " + userGuess + ", ";	    
-		}
-	}
-}
+	var char = userGuess;
+		letters.innerHTML = letters.innerHTML + " " + userGuess + ", ";	
 
 //Generate underscores depending on the length of the answer
-
-function generateUnderscore() {
-	var numberOfUnderscores = answer.length;
-	var storeUnderscore = "";
-	for ( var i = 0; i < numberOfUnderscores; i++) {
-		storeUnderscore = storeUnderscore + "_" + " ";
-	}
-	document.querySelector('#answer').innerHTML = storeUnderscore;
-}
-generateUnderscore();
-
-//Replace the underscore with the letter
-
-var char = userGuess;
-function fillInWordWithLetter(char) {
-	for ( var k = 0; k < answer.length; k++) {
-		if ( answer.charAt(k) == char) {
-			displayedAnswer[k] = char;
+		function generateUnderscore() {
+			var numberOfUnderscores = answer.length;
+			var storeUnderscore = "";
+			for ( var i = 0; i < numberOfUnderscores; i++) {
+				storeUnderscore = storeUnderscore + "_" + " ";
+			}
+			document.querySelector('#answer').innerHTML = storeUnderscore;
 		}
-		document.querySelector('#answer').innerHTML = displayedAnswer[k];
-	}
+		generateUnderscore();
+
+//Replace the underscore with the letter and update guesses left
+		function fillInWordWithLetter(char) {
+			for ( var k = 0; k < answer.length; k++) {
+				if ( answer.charAt(k) == char) {
+					displayedAnswer[k] = char;
+					document.querySelector('#answer').innerHTML = displayedAnswer[k];
+				}
+				else {
+					guessesLeft--;
+					document.querySelector('#guessesLeft').innerHTML = "Number of guesses remained: " + guessesLeft;
+				}
+				if ( guessesLeft == 0) {
+					alert(Game Over!);
+				}
+			}
+		}
+		fillInWordWithLetter(char);
 }
 
+//Each attempt by the user will decrease the number of guessLeft. And then alert the "Game Over" message.
+//After the user finish the answer, the wins will go up. And change to the answer image.
+//If "Game Over", then reset the game to 0 win.
+//If user wins the game, then reset the game to 1+ win and continue the game.
 
 
-var updateGuessesLeft = function() {
-	document.querySelector('#guessLeft').innerHTML = "Number of guesses remained: " + guessesLeft;
-};
-
-
-var updateGuessesSoFar = function() {
-  document.querySelector('#usedGuesses').innerHTML = "Letters typed: " + guessedLetters.join(', ');
-};
 
 var reset = function() {
   totalGuesses = 5;
