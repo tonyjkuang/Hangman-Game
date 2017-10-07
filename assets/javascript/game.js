5f79723a135411c104711adc1ed6ab20957fda8a
Tony Kuang <tonyjkuang@gmail.com>
2017-10-06 21:53:19 -0500

    <COMMENT>

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -3,64 +3,66 @@ var computerChoices = ["warriors","cavaliers","lakers","spurs","knicks","celtics
 "clippers","76ers","nets","pelicans","mavericks","wizards",
 "blazers","kings","suns","pistons","pacers","hawks",
 "timberwolves","nuggets","grizzlies","hornets","magic","jazz"];
-var guesses = 5;
+var key = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z",
+"1","2","3","4","5","6","7","8","9","0"];
+var guesses = 7;
 var wins = 0;
-var guessedLetters = [];
-var displayedAnswer = [];
-var letterToGuess = null;
-
+var chosenLetters =[];
+var misses = [];
 var answer = computerChoices[Math.floor(Math.random() * computerChoices.length)];
+var answerTokens = answer.split("");
+var storeUnderscore = generateUnderscore(answer);
+var guessMatches = [];
 
-//Generate underscores depending on the length of the answer
-		function generateUnderscore() {
-			var numberOfUnderscores = answer.length;
-			var storeUnderscore = "";
+//Generate underscores depending on the length of the answer//Initialize the match with underscore
+		function generateUnderscore(answer) {
+			var numberOfUnderscores = answerTokens.length;
+			var storeUnderscore = [];
 			for ( var i = 0; i < numberOfUnderscores; i++) {
-				storeUnderscore = storeUnderscore + "_" + " ";
+				storeUnderscore.push("_"); 	
 			}
-			document.querySelector('#answer').innerHTML = storeUnderscore;
+			document.querySelector('#answer').innerHTML = storeUnderscore.join(" ");
+			return storeUnderscore;
 		}
-		generateUnderscore();
 
-// Show letters user input
+//Replace the underscore with the letter and update guesses left and log in unmatched guesses
 document.onkeyup = function(event) {
-	var letters = document.querySelector('#usedGuesses');
 	var userGuess = event.key;
-	var char = userGuess;
-		letters.innerHTML = letters.innerHTML + " " + userGuess + ", ";	
-
-//Replace the underscore with the letter and update guesses left
-		function fillInWordWithLetter(char) {
-			for ( var k = 0; k < answer.length; k++) {
-				if ( answer.charAt(k) == char) {
-					displayedAnswer[k] = char;
-					document.querySelector('#answer').innerHTML = displayedAnswer[k]; //this does not replace the underscore
-				}
-				
-				else if ( answer.charAt(k) !== char ) {
-					guesses--; //Guesses do not start with 5.
-				}
-				document.querySelector('#guessesLeft').innerHTML = guesses;
-				
+	var found = false;
+		for ( var h = 0; h < answerTokens.length; h++ ) {
+			if ( userGuess == answerTokens[h] ) {
+				var found = true;
+				guessMatches[h] = answerTokens[h];
+				document.querySelector('#answer').innerHTML = guessMatches.join(" "); //replace the underscore with the right guesses
+				document.querySelector('#guessesLeft').innerHTML = guesses; //update guesses remaining
 			}
 		}
-		fillInWordWithLetter(char);
+
+        if (!found) {
+            misses.push(userGuess);
+            guesses = guesses - 1;
+            document.querySelector('#usedGuesses').innerHTML = misses + ", "; //log in incorrect guesses
+            document.querySelector('#guessesLeft').innerHTML = guesses; //update guesses remaining
+        }
+			
+	
+		if ( guesses == 0 ) {
+			document.querySelector('#answer').innerHTML = answerTokens.join(" ");
+			alert("Game Over!");
+		}
+
+		if ( numberOfUnderscores == 0 ) {
+			document.querySelector('#wins').innerHTML = wins++;
+			//change the answer image
+			alert("You win!");
+		}
+		return;
+	}
+
+function reset() {
+    document.getElementById('#btn').innerHTML = window.location.reload();
 }
 
-//Each attempt by the user will decrease the number of guessLeft. And then alert the "Game Over" message.
-//After the user finish the answer, the wins will go up. And change to the answer image.
-//If "Game Over", then reset the game to 0 win.
-//If user wins the game, then reset the game to 1+ win and continue the game.
 
 
-
-var reset = function() {
-  totalGuesses = 5;
-  guessesLeft = 5;
-  guessedLetters = [];
-
-  updateLetterToGuess();
-  updateGuessesLeft();
-  updateGuessesSoFar();
-}
 
