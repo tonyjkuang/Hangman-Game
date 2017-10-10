7d9c33a3482c50e1a015cee5e26beb45f970ff0e
Tony Kuang <tonyjkuang@gmail.com>
2017-10-10 07:50:57 -0500

    <COMMENT>

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,3 +1,4 @@
+//Global scope
 var computerChoices = ["warriors","cavaliers","lakers","spurs","knicks","celtics",
 "thunder","bulls","rockets","raptors","heat","bucks",
 "clippers","76ers","nets","pelicans","mavericks","wizards",
@@ -5,64 +6,143 @@ var computerChoices = ["warriors","cavaliers","lakers","spurs","knicks","celtics
 "timberwolves","nuggets","grizzlies","hornets","magic","jazz"];
 var key = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z",
 "1","2","3","4","5","6","7","8","9","0"];
-var guesses = 7;
 var wins = 0;
-var chosenLetters =[];
-var misses = [];
+var guesses = 7;
+var allGuesses =[]; //this is all the guesses user inputs.
+var misses = []; //wrong guesses user makes.
 var answer = computerChoices[Math.floor(Math.random() * computerChoices.length)];
 var answerTokens = answer.split("");
-var storeUnderscore = generateUnderscore(answer);
-var guessMatches = [];
+document.querySelector('#answer').innerHTML = generateUnderscore(answerTokens,allGuesses).join(" ");
+document.querySelector('#wins').innerHTML = "Wins: " + wins;
+
 
 //Generate underscores depending on the length of the answer//Initialize the match with underscore
-		function generateUnderscore(answer) {
-			var numberOfUnderscores = answerTokens.length;
-			var storeUnderscore = [];
-			for ( var i = 0; i < numberOfUnderscores; i++) {
-				storeUnderscore.push("_"); 	
+function generateUnderscore(answerTokens, allGuesses) {
+	var partialGuess = [];
+	var isCorrect = false;
+	for ( var i = 0; i < answerTokens.length; i++) {
+		for ( var j = 0; j < allGuesses.length; j++) {
+			if ( allGuesses[j] == answerTokens[i] ) {
+				partialGuess.push(allGuesses[j]);
+				isCorrect = true;
+				break;
 			}
-			document.querySelector('#answer').innerHTML = storeUnderscore.join(" ");
-			return storeUnderscore;
 		}
+		if ( isCorrect ) {
+			isCorrect = false;
+		}
+		else {
+			partialGuess.push("_");
+		}
+	}
+	return partialGuess;
+}
 
 //Replace the underscore with the letter and update guesses left and log in unmatched guesses
 document.onkeyup = function(event) {
 	var userGuess = event.key;
 	var found = false;
-		for ( var h = 0; h < answerTokens.length; h++ ) {
-			if ( userGuess == answerTokens[h] ) {
+		for ( var h = 0; h < allGuesses.length; h++ ) {
+			if ( userGuess == allGuesses[h] ) {
 				var found = true;
-				guessMatches[h] = answerTokens[h];
-				document.querySelector('#answer').innerHTML = guessMatches.join(" "); //replace the underscore with the right guesses
-				document.querySelector('#guessesLeft').innerHTML = guesses; //update guesses remaining
+				alert("You have already guessed " + userGuess + "!");
 			}
 		}
 
-        if (!found) {
-            misses.push(userGuess);
-            guesses = guesses - 1;
-            document.querySelector('#usedGuesses').innerHTML = misses + ", "; //log in incorrect guesses
-            document.querySelector('#guessesLeft').innerHTML = guesses; //update guesses remaining
-        }
-			
-	
-		if ( guesses == 0 ) {
-			document.querySelector('#answer').innerHTML = answerTokens.join(" ");
-			alert("Game Over!");
-		}
+		if (!found) {
+			allGuesses.push(userGuess);
+			document.querySelector('#usedGuesses').innerHTML = misses.join(", "); //log in incorrect guesses
+			var isCorrect = false;
+			for ( var k = 0; k < answerTokens.length; k++ ) {
+				if ( userGuess == answerTokens[k] ) {
+					isCorrect = true;
+					break;
+				}
+			}
+			if (!isCorrect) {
+				misses.push(userGuess);
+				guesses = guesses - 1;
+				document.querySelector('#usedGuesses').innerHTML = misses.join(", "); //log in incorrect guesses
+				document.querySelector('#guessesLeft').innerHTML = guesses; //update guesses remaining
+				if ( guesses == 0  ) {
+					alert("The answer is " + answer + "!" + " Game Over!");
+					guesses = 7;
+					allGuesses =[]; //this is all the guesses user inputs.
+					misses = []; //wrong guesses user makes.
+					answer = computerChoices[Math.floor(Math.random() * computerChoices.length)];
+					answerTokens = answer.split("");
+					document.querySelector('#usedGuesses').innerHTML = misses;
+					document.querySelector('#guessesLeft').innerHTML = guesses; //update guesses remaining
+					document.querySelector('#answer').innerHTML = generateUnderscore(answerTokens,allGuesses).join(" ");		
+				}
+			}
+		else {
+			document.querySelector('#answer').innerHTML = generateUnderscore(answerTokens,allGuesses).join(" ");
+						//this block has to be executed no matter what so that you display the correct data on the user-interface
 
-		if ( numberOfUnderscores == 0 ) {
-			document.querySelector('#wins').innerHTML = wins++;
-			//change the answer image
-			alert("You win!");
 		}
-		return;
+			countUnderscores = 0;
+			partialAnswer = generateUnderscore(answerTokens, allGuesses)
+			for ( var z = 0; z < partialAnswer.length; z++) {
+                if ( partialAnswer[z] == "_" ) {
+                    countUnderscores = countUnderscores + 1;
+				}
+				if ( generateUnderscore(answerTokens, allGuesses)[z] == "_" ) {
+                    countUnderscores = countUnderscores;
+                
+              		if ( countUnderscores == 0 ) {
+                        alert("jjj");
+                        
+                        wins = wins + 1;
+                        //guesses = 7;
+                        //allGuesses = [];
+                        //misses = [];
+                        //answer = computerChoices[Math.floor(Math.random() * computerChoices.length)];
+                        //answerTokens = answer.split("");
+                        //document.querySelector('#usedGuesses').innerHTML = misses;
+                        //document.querySelector('#guessesLeft').innerHTML = guesses; //update guesses remaining
+                        //document.querySelector('#answer').innerHTML = generateUnderscore(answerTokens,allGuesses).join(" ");  
+                        //document.querySelector('#wins').innerHTML = wins;	
+                        
+					
+                    }
+				}
+
+			}
+	}
+}
+	
+
+		/*
+		var countUnderscores = 0;
+			for ( var z = 0; z < generateUnderscore(answerTokens, allGuesses).length; z++) {
+				if ( generateUnderscore(answerTokens, allGuesses)[z] == "_" ) {
+					countUnderscores = countUnderscores + 1;
+					}
+				else if ( countUnderscores == 0 ) {
+					alert("You won! Congratulations!");
+					wins++;
+					guesses = 7;
+					allGuesses = [];
+					misses = [];
+					answer = computerChoices[Math.floor(Math.random() * computerChoices.length)];
+					answerTokens = answer.split("");
+					document.querySelector('#usedGuesses').innerHTML = misses;
+					document.querySelector('#guessesLeft').innerHTML = guesses; //update guesses remaining
+					document.querySelector('#answer').innerHTML = generateUnderscore(answerTokens,allGuesses).join(" ");	
+					document.querySelector('#wins').innerHTML = wins;
+				}
+			}
 	}
 
-function reset() {
-    document.getElementById('#btn').innerHTML = window.location.reload();
 }
 
-
-
-
+			
+	
+/*
+function reset(didWin) {
+	if ( didWin == true ) {
+		wins++;
+	}   
+}
+*/
